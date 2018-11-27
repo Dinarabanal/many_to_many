50acf73d2b4f2a3a7eb5680b9b2c1655eb428281
Dina <dmrabanal@gmail.com>
2018-11-27 15:53:03 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/many_to_many/view/BaseProject.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.many_to_many.view;
+
+import java.net.URI;
+import java.util.Date;
+
+public interface BaseProject {
+
+  long getId();
+
+  Date getCreated();
+
+  String getName();
+
+  URI getHref();
+
+
+}
